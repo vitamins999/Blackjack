bf9272438e40383cfde3d8cde341d8a384c2dfa2
Jools Barnett <vitamins999@gmail.com>
2022-05-18 12:16:52 +0100

    Added and implemented classes for card/deck/draw pile.

---
+++ Blackjack/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class Deck
+    {
+        private List<Card> allCards = new List<Card>();
+
+        public Deck()
+        {
+            allCards.AddRange(GenerateSuite("Hearts"));
+            allCards.AddRange(GenerateSuite("Diamonds"));
+            allCards.AddRange(GenerateSuite("Spades"));
+            allCards.AddRange(GenerateSuite("Clubs"));
+        }
+
+        private List<Card> GenerateSuite(string suiteName)
+        {
+            List<Card> suite = new List<Card>();
+
+            suite.Add(new Card("Two", suiteName, 2));
+            suite.Add(new Card("Three", suiteName, 3));
+            suite.Add(new Card("Four", suiteName, 4));
+            suite.Add(new Card("Five", suiteName, 5));
+            suite.Add(new Card("Six", suiteName, 6));
+            suite.Add(new Card("Seven", suiteName, 7));
+            suite.Add(new Card("Eight", suiteName, 8));
+            suite.Add(new Card("Nine", suiteName, 9));
+            suite.Add(new Card("Ten", suiteName, 10));
+            suite.Add(new Card("Jack", suiteName, 10));
+            suite.Add(new Card("Queen", suiteName,10));
+            suite.Add(new Card("King", suiteName, 10));
+            suite.Add(new Card("Ace", suiteName, 11));
+
+            return suite;
+        }
+
+        public List<Card> GetAllCardsInDeck()
+        {
+            return allCards;
+        }
+    }
+}
---
+++ Blackjack/DrawPile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    internal class DrawPile
+    {
+        Queue<Card> drawPile = new Queue<Card>();
+
+        public DrawPile(int numberOfDecks)
+        {
+            List<Card> unshuffledPile = new List<Card>();
+
+            for(int i = 0; i < numberOfDecks; i++)
+            {
+                Deck deck = new Deck();
+                unshuffledPile.AddRange(deck.GetAllCardsInDeck());
+            }
+
+            Shuffle(unshuffledPile);
+        }
+
+        private void Shuffle(List<Card> unshuffledPile)
+        {
+            Random rand = new Random();
+            int totalCards = unshuffledPile.Count;
+
+            while (totalCards > 0)
+            {
+                int index = rand.Next(totalCards);
+
+                drawPile.Enqueue(unshuffledPile[index]);
+                unshuffledPile.RemoveAt(index);
+
+                totalCards--;
+            }
+        }
+
+        public Card DrawCard()
+        {
+            return drawPile.Dequeue();
+        }
+    }
+}
