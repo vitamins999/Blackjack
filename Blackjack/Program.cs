fe5b9f251d84f0e22b4c9ac7fceb8d2b41b4d227
Jools Barnett <vitamins999@gmail.com>
2022-05-18 13:46:52 +0100

    Implemented dealer AI and stand/hit mechanic.  Single game loop works perfectly.

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -9,44 +9,82 @@ namespace Blackjack
             int playerHand = 0;
             int dealerHand = 0;
 
-            while (true)
+            bool playerStand = false;
+            string winner = "Draw";
+
+            while (playerStand == false && dealerHand < 17)
             {
                 DrawPile drawPile = new DrawPile(1);
 
-                Console.WriteLine("Deal a new card? (y/n)\n");
-                string dealNewCard = Console.ReadLine();
-
-                if (dealNewCard.Equals("n", StringComparison.OrdinalIgnoreCase))
+                if (playerStand == false)
                 {
-                    break;
+                    Console.WriteLine("Deal a new card? (y/n)\n");
+                    string dealNewCard = Console.ReadLine();
+
+                    if (dealNewCard.Equals("y", StringComparison.OrdinalIgnoreCase))
+                    {
+                        playerHand = AddCardToHand("Player", playerHand, drawPile);
+
+                        if (playerHand == 21)
+                        {
+                            Console.WriteLine("Blackjack!");
+                            winner = "Player";
+                            break;
+                        }
+                        else if (playerHand > 21)
+                        {
+                            Console.WriteLine("Bust!");
+                            winner = "Dealer";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        playerStand = true;
+                    }
                 }
 
-                playerHand = AddCardToHand("Player", playerHand, drawPile);
+                if (dealerHand < 17)
+                {
+                    dealerHand = AddCardToHand("Dealer", dealerHand, drawPile);
 
-                if (playerHand == 21)
-                {
-                    Console.WriteLine("Blackjack! You win!");
-                    break;
-                } else if (playerHand > 21)
-                {
-                    Console.WriteLine("Bust! You lose!");
-                    break;
-                }
-
-                dealerHand = AddCardToHand("Dealer", dealerHand, drawPile);
-
-                if (dealerHand == 21)
-                {
-                    Console.WriteLine("Blackjack! You lose!");
-                    break;
-                } else if (dealerHand > 21)
-                {
-                    Console.WriteLine("Dealer bust! You win!");
-                    break;
+                    if (dealerHand == 21)
+                    {
+                        Console.WriteLine("Blackjack!");
+                        winner = "Dealer";
+                        break;
+                    }
+                    else if (dealerHand > 21)
+                    {
+                        Console.WriteLine("Dealer bust!");
+                        winner = "Player";
+                        break;
+                    }
                 }
             }
 
-            Console.WriteLine("\nGame over.  And yes, I know that's not how blackjack works. This is just to have a basic gameplay loop that will be made better.");
+            Console.WriteLine("\n**Final Scores**");
+            Console.WriteLine($"\nPlayer: { playerHand }");
+            Console.WriteLine($"\nDealer: {dealerHand}\n");
+
+            if (playerHand > dealerHand && playerHand <= 21)
+            {
+                winner = "Player";
+            } else if (dealerHand > playerHand && dealerHand <= 21)
+            {
+                winner = "Dealer";
+            }
+
+            if (winner.Equals("Player"))
+            {
+                Console.WriteLine("\nCongratulations! You win!");
+            } else if (winner.Equals("Dealer"))
+            {
+                Console.WriteLine("\nToo bad! Dealer wins!");
+            } else
+            {
+                Console.WriteLine("\nWell how about that? It's a draw!");
+            }
         }
 
         static int AddCardToHand(string handOwner, int handValue, DrawPile drawPile)
