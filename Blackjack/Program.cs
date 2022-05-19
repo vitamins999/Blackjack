1d5c7beda84f96b9d2fee15a0c4894be734b1ca1
Jools Barnett <vitamins999@gmail.com>
2022-05-19 12:13:11 +0100

    Implemented abilty to play multiple games until user quits.

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,10 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Blackjack
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            string amountOfDecksString;
+            bool isNumber = false;
+            int amountOfDecksNumber = 0;
+            bool keepPlaying = true;
+
+            Console.WriteLine("Welcome to BLACKJACK!\n\n");
+
+            while (!isNumber || amountOfDecksNumber <= 0)
+            {
+                Console.WriteLine("How many decks would you like to play with?\n");
+                amountOfDecksString = Console.ReadLine();
+
+                isNumber = int.TryParse(amountOfDecksString, out amountOfDecksNumber);
+
+                if (!isNumber || amountOfDecksNumber <= 0)
+                {
+                    Console.WriteLine($"{amountOfDecksString} is not a valid number! Please type in a whole number and try again.\n");
+                }
+            }
+            
+            DrawPile drawPile = new DrawPile(amountOfDecksNumber);
+            Console.WriteLine("");
+
+            while (keepPlaying)
+            {
+                drawPile = PlayGame(drawPile);
+
+                Console.WriteLine("\nKeep playing? (y/n)\n");
+                string keepPlayingString = Console.ReadLine().Trim();
+
+                keepPlaying = keepPlayingString.Equals("y", StringComparison.OrdinalIgnoreCase);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Thanks for playing!");
+        }
+
+        static DrawPile PlayGame(DrawPile drawPile)
         {
             int playerHand = 0;
             int dealerHand = 0;
@@ -14,12 +54,11 @@ namespace Blackjack
 
             while (playerStand == false && dealerHand < 17)
             {
-                DrawPile drawPile = new DrawPile(1);
 
                 if (playerStand == false)
                 {
                     Console.WriteLine("Deal a new card? (y/n)\n");
-                    string dealNewCard = Console.ReadLine();
+                    string dealNewCard = Console.ReadLine().Trim();
 
                     if (dealNewCard.Equals("y", StringComparison.OrdinalIgnoreCase))
                     {
@@ -60,17 +99,21 @@ namespace Blackjack
                         winner = "Player";
                         break;
                     }
+                } else
+                {
+                    continue;
                 }
             }
 
             Console.WriteLine("\n**Final Scores**");
-            Console.WriteLine($"\nPlayer: { playerHand }");
+            Console.WriteLine($"\nPlayer: {playerHand}");
             Console.WriteLine($"\nDealer: {dealerHand}\n");
 
             if (playerHand > dealerHand && playerHand <= 21)
             {
                 winner = "Player";
-            } else if (dealerHand > playerHand && dealerHand <= 21)
+            }
+            else if (dealerHand > playerHand && dealerHand <= 21)
             {
                 winner = "Dealer";
             }
@@ -78,13 +121,17 @@ namespace Blackjack
             if (winner.Equals("Player"))
             {
                 Console.WriteLine("\nCongratulations! You win!");
-            } else if (winner.Equals("Dealer"))
+            }
+            else if (winner.Equals("Dealer"))
             {
                 Console.WriteLine("\nToo bad! Dealer wins!");
-            } else
+            }
+            else
             {
                 Console.WriteLine("\nWell how about that? It's a draw!");
             }
+
+            return drawPile;
         }
 
         static int AddCardToHand(string handOwner, int handValue, DrawPile drawPile)
